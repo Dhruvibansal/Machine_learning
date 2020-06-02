b1ace06a0f9576985437fa5cb1c009f7d5e4d8b2
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-06-02 13:55:33 +0530

    Add files via upload

---
+++ sine_wave.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+x= np.arange(-pi,pi,0.001)
+x=list(x)
+y=[]
+for i in x:
+   y.append(sin(i))
+
+plt.plot(x,y)   
+plt.show()
